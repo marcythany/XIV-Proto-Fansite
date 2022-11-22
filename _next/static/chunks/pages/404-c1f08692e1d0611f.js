43c37c5ccaf4ba14b582f3933b419240b5a2690e
marcythany <thanymoon1@gmail.com>
2022-11-22 15:08:15 -0300

    update

--- _next/static/chunks/pages/404-c1f08692e1d0611f.js
+++ _next/static/chunks/pages/404-c1f08692e1d0611f.js
@@ -1,1 +1,30 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{8893:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return e(6109)}])},6109:function(n,t,e){"use strict";e.r(t);var u=e(1527);let o=()=>(0,u.jsx)("div",{className:"text-center text-gray-600 font-semibold mt-20 text-2xl",children:"Oops! page not found \uD83D\uDE22"});t.default=o}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8893)}),_N_E=n.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+	[197],
+	{
+		8893: function (n, t, e) {
+			(window.__NEXT_P = window.__NEXT_P || []).push([
+				"/404",
+				function () {
+					return e(6109);
+				},
+			]);
+		},
+		6109: function (n, t, e) {
+			"use strict";
+			e.r(t);
+			var u = e(1527);
+			let o = () =>
+				(0, u.jsx)("div", {
+					className: "text-center text-gray-600 font-semibold mt-20 text-2xl",
+					children: "Oops! page not found \uD83D\uDE22",
+				});
+			t.default = o;
+		},
+	},
+	function (n) {
+		n.O(0, [774, 888, 179], function () {
+			return n((n.s = 8893));
+		}),
+			(_N_E = n.O());
+	},
+]);
--- _next/static/chunks/pages/breadcumbs-512edea0af9f6fc1.js
+++ _next/static/chunks/pages/breadcumbs-512edea0af9f6fc1.js
@@ -1,1 +1,37 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[967],{5697:function(e,i,n){(window.__NEXT_P=window.__NEXT_P||[]).push(["/breadcumbs",function(){return n(1732)}])},1732:function(e,i,n){"use strict";n.r(i);var r=n(1527);n(959);let s=e=>(0,r.jsx)("div",{className:"grid w-[100vw] place-items-center text-lg breadcrumbs text-primary",children:(0,r.jsxs)("ul",{children:[(0,r.jsx)("li",{children:(0,r.jsx)("a",{children:"Home"})}),(0,r.jsx)("li",{children:(0,r.jsx)("a",{children:"Raibu Reta"})}),(0,r.jsx)("li",{children:(0,r.jsx)("a",{children:"Guides"})})]})});i.default=s}},function(e){e.O(0,[774,888,179],function(){return e(e.s=5697)}),_N_E=e.O()}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+	[967],
+	{
+		5697: function (e, i, n) {
+			(window.__NEXT_P = window.__NEXT_P || []).push([
+				"/breadcumbs",
+				function () {
+					return n(1732);
+				},
+			]);
+		},
+		1732: function (e, i, n) {
+			"use strict";
+			n.r(i);
+			var r = n(1527);
+			n(959);
+			let s = (e) =>
+				(0, r.jsx)("div", {
+					className: "grid w-[100vw] place-items-center text-lg breadcrumbs text-primary",
+					children: (0, r.jsxs)("ul", {
+						children: [
+							(0, r.jsx)("li", {children: (0, r.jsx)("a", {children: "Home"})}),
+							(0, r.jsx)("li", {children: (0, r.jsx)("a", {children: "Raibu Reta"})}),
+							(0, r.jsx)("li", {children: (0, r.jsx)("a", {children: "Guides"})}),
+						],
+					}),
+				});
+			i.default = s;
+		},
+	},
+	function (e) {
+		e.O(0, [774, 888, 179], function () {
+			return e((e.s = 5697));
+		}),
+			(_N_E = e.O());
+	},
+]);
