b842cefdeceb17a0fb18ff971768d697d4ebc72b
marcythany <thanymoon1@gmail.com>
2022-11-22 15:00:18 -0300

    Finished

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-	return (
-		<div className="text-center text-gray-600 font-semibold mt-20 text-2xl">
-			Oops! page not found 😢
-		</div>
-	);
-};
-
-export default NotFound;
